69c097f14f15f8216930e54c693e3d3e2af64f93
盛羽_Sakae <60038013+Taketo-i@users.noreply.github.com>
2021-09-15 16:43:32 +0800

    Add files via upload

---
+++ segmentation.py
@@ -0,0 +1,18 @@
+import os
+
+
+
+def segment(root):
+    '''
+    Break each row of the text into a list of individual words
+    without '(', ')', ';', ';', '{' and '}',
+    then merge those lists into one list
+    '''
+    f = open(root, "r")
+    lines = f.readlines()
+    result = []
+    for line in lines:
+        line = line.translate({ord(i): " " for i in "();:{}"})
+        line_split = line.split()
+        result.extend(line_split)
+    return result
