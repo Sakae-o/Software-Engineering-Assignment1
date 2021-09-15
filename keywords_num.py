f2dce736da17e9c5d899639d2fdb3ccf83f4b91b
盛羽_Sakae <60038013+Taketo-i@users.noreply.github.com>
2021-09-15 16:43:03 +0800

    Update keywords_num.py

--- keywords_num.py
+++ keywords_num.py
@@ -6,21 +6,16 @@ keywords = ["auto",	"break", "case", "char", "const", "continue", "default", "do
 num = [0 for i in range(32)]
 
 
-def statistics(lines):
+def statistics(words):
     ''' Count the number of each keyword in the text. '''
-    for line in lines:
-        line = line.translate({ord(i): " " for i in "();:{}"})
-        line_split = line.split()
-        print(line_split)
-        for i in range(32):
-            num[i] += line_split.count(keywords[i])
+    for i in range(32):
+        num[i] = words.count(keywords[i])
 
 
-def compute_num(lines):
+def compute_num(words):
     ''' Compute the total number of the keywords '''
-    statistics(lines)
+    statistics(words)
     result = 0
     for i in num:
         result += i
-    print("\narray num:", num)
     return result
