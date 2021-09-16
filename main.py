612279c6d12a7d28e90413a46ae5f1ce29594615
盛羽_Sakae <60038013+Taketo-i@users.noreply.github.com>
2021-09-17 02:05:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 import segmentation
 import keywords_num
-import count_of_switch
+import count_of_switch as cnt_s
+import count_of_if_else as cnt_ie
 
 
-
-root = "test.cpp"
+root = "test_if_else.cpp"
 words = segmentation.segment(root)
 print("total num:",keywords_num.compute_num(words))
-count_of_switch.count(words)
+cnt_s.count(words)
+print("\nif-else num:", cnt_ie.count(words))
