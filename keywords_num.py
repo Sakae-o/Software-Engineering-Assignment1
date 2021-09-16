75fd874b8be7af7199c391dc0b014ac5c5c78827
盛羽_Sakae <60038013+Taketo-i@users.noreply.github.com>
2021-09-17 02:06:57 +0800

    Update keywords_num.py

--- keywords_num.py
+++ keywords_num.py
@@ -1,19 +1,20 @@
 keywords = ["auto",	"break", "case", "char", "const", "continue", "default", "do",
             "double", "else", "enum", "extern",	"float", "for",	"goto",	"if",
             "int",	"long",	"register",	"return", "short", "signed", "sizeof", "static",
-            "struct", "switch",	"typedef", "union",	"unsigned",	"void",	"volatile",	"while"]
+            "struct", "switch",	"typedef", "union",	"unsigned",	"void",	"volatile",
+            "while", "do{", "else{", "do{}", "else{}"]
 
-num = [0 for i in range(32)]
+num = [0 for i in range(36)]
 
 
 def statistics(words):
     ''' Count the number of each keyword in the text. '''
-    for i in range(32):
+    for i in range(36):
         num[i] = words.count(keywords[i])
 
 
 def compute_num(words):
-    ''' Compute the total number of the keywords '''
+    ''' Compute the total number of the keywords. '''
     statistics(words)
     result = 0
     for i in num:
