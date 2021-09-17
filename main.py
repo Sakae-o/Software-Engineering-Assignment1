21394092791f3ddd372d632af2a99dee1453ada5
盛羽_Sakae <60038013+Taketo-i@users.noreply.github.com>
2021-09-17 21:32:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,23 @@ import count_of_switch as cnt_s
 import count_of_if_else as cnt_ie
 
 
-root = "test_file/test.cpp"
+
+print("Please input the path of a cpp file:", end=" ")
+root = input()
+print("Please input the execution level:", end=" ")
+level = int(input())
+
 words = segmentation.segment(root)
-print("total num:",keywords_num.compute_num(words))
-cnt_s.count(words)
-print("\nif-else num:", cnt_ie.count(words))
+total_num = keywords_num.compute_num(words)
+num_i_e = cnt_ie.count_if_else(words)
+num_i_e_e = cnt_ie.count_else(words) - num_i_e
+
+if(level >= 1):
+    print("total num:", total_num)
+if(level >= 2):
+    cnt_s.count(words)
+if (level >= 3):
+    print("\nif-else num:", num_i_e)
+if(level == 4):
+    print("if-elseif-else num:", num_i_e_e)
+
