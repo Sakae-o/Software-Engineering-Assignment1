ac110b27fac5ea5694b27898df357774547964f8
盛羽_Sakae <60038013+Sakae-o@users.noreply.github.com>
2021-09-20 03:24:37 +0800

    Update count_of_switch.py

--- count_of_switch.py
+++ count_of_switch.py
@@ -7,16 +7,17 @@ def count(words):
     Output the number of switch structure
     and the number of "case" each switch structure has.
     '''
-    print("switch num:", num[25])
+    case_list = []
 
-    print("case num:", end=" ")
     if (num[25] == 0):
-        print("0", end=" ")
-        return
+        case_list.append(0)
+        return 0, case_list
+
     case_num = 0
     for i in range(words.index("switch") + 1, words.__len__()):
         if (words[i] == "switch" or i == words.__len__() - 1):
-            print(case_num, end=" ")
+            case_list.append(case_num)
             case_num = 0
         elif (words[i] == "case"):
             case_num += 1
+    return num[25], case_list
