db26ae66a26d819ad59838a025c08850c53e69cc
盛羽_Sakae <60038013+Sakae-o@users.noreply.github.com>
2021-09-20 03:23:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,22 @@ level = int(input())
 
 words = segmentation.segment(root)
 total_num = keywords_num.compute_num(words)
+num_s, num_c = cnt_s.count(words)
 num_i_e = cnt_ie.count_if_else(words)
 num_i_e_e = cnt_ie.count_else(words) - num_i_e
 
-if (level >= 1):
+if(level >= 1):
     print("total num:", total_num)
-if (level >= 2):
-    cnt_s.count(words)
+if(level >= 2):
+    print("switch num:", num_s)
+    print("case num:", end=" ")
+    for c in num_c:
+        print(c, end=" ")
 if (level >= 3):
     print("\nif-else num:", num_i_e)
-if (level == 4):
+if(level == 4):
     print("if-elseif-else num:", num_i_e_e)
+
+
+
 
